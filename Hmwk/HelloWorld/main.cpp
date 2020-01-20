b77e38a92caba7d74c8fc79407a2cd889a6c9242
KChung5 <60079549+KChung5@users.noreply.github.com>
2020-01-20 14:35:08 -0800

    Update main.cpp

--- Hmwk/HelloWorld/main.cpp
+++ Hmwk/HelloWorld/main.cpp
@@ -1,20 +1,6 @@
-/* 
- * File:   main.cpp
- * Author: Dr. Mark E. Lehr
- * Created on January 6, 2020, 12:40 PM
- * Purpose:  First Program "Hello World"
- */
-
 //System Libraries
 #include <iostream>
 using namespace std;
-
-//User Libraries
-
-//Global Constants - No Global Variables
-//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
-
-//Function Prototypes
 
 //Execution Begins Here
 int main(int argc, char** argv) {
